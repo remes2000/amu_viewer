726f40eb9a8bfb3a5716a67e9870d57b0bc5acba
remes2000 <nieruchalskimichal@gmail.com>
2021-11-25 23:50:15 +0100

    Read client udp port and send message every second, add generate jpg frame method prototype

--- host/host.c
+++ host/host.c
@@ -6,6 +6,12 @@
 #include <ifaddrs.h>
 #include <string.h>
 #include <unistd.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <jpeglib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define DEFAULT_TCP_PORT 1337
 #define DEFAULT_UDP_PORT DEFAULT_TCP_PORT + 1
@@ -31,7 +37,94 @@ int find_interface(char * interface, struct in_addr ** address);
 void say_hello(struct sockaddr_in * tcp_host_address, struct sockaddr_in * udp_host_address, unsigned int access_code);
 void handle_connection(int host_socket_tcp, int host_socket_udp, int client_socket_tcp, struct sockaddr_in * tcp_client_address, unsigned int access_code);
 int authorize(int client_socket_tcp, unsigned int access_code);
-unsigned int read_access_code(int client_socket_tcp);
+void set_udp_client_address(int client_socket_tcp, struct in_addr client_addr, struct sockaddr_in * udp_client_address);
+unsigned int read_integer(int client_socket_tcp);
+int begin_screen_broadcast(int host_socket_udp, struct sockaddr_in * udp_client_address);
+void send_frame(int host_socked_udp, struct sockaddr_in * udp_client_address);
+
+void write_jpeg_file(char * filename, int quality) {
+    /* x11 code */
+    Display *disp;
+    Window root;
+    XWindowAttributes gwa;
+    int scr;
+
+    // the display points to X server
+    disp = XOpenDisplay(0);
+    // the screen refers to which screen of display to use
+    scr = DefaultScreen(disp);
+    // the window controls the actual window itself
+    root = DefaultRootWindow(disp);
+
+    XGetWindowAttributes(disp, root, &gwa);
+    int width = gwa.width;
+    int height = gwa.height;
+    XImage *image = XGetImage(disp, root, 0, 0, width, height, AllPlanes, ZPixmap);
+
+    unsigned long red_mask = image->red_mask;
+    unsigned long green_mask = image->green_mask;
+    unsigned long blue_mask = image->blue_mask;
+
+    printf(" to allocate %d \n", width*height*3*sizeof(unsigned char));
+    unsigned char * image_buffer = (char *) malloc(width*height*3*sizeof(unsigned char));
+
+    long position = 0;
+
+    for(int y=0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            unsigned long pixel = XGetPixel(image, x, y);
+            unsigned char blue = pixel & blue_mask;
+            unsigned char green = (pixel & green_mask) >> 8;
+            unsigned char red = (pixel & red_mask) >> 16;
+
+            image_buffer[position] = red;
+            image_buffer[position+1] = green;
+            image_buffer[position+2] = blue;
+
+            position += 3;
+        }
+    }
+
+    /* libjpg code */
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE * outfile;
+    JSAMPROW row_pointer[1];
+    int row_stride;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    if ((outfile = fopen(filename, "wb")) == NULL) {
+        fprintf(stderr, "can't open %s\n", filename);
+        exit(1);
+    }
+//    jpeg_stdio_dest(&cinfo, outfile);
+    unsigned char * result;
+    unsigned long size;
+    jpeg_mem_dest(&cinfo, &result, &size);
+
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, quality, TRUE);
+    jpeg_start_compress(&cinfo, TRUE);
+
+    row_stride = width * 3;	/* JSAMPLEs per row in image_buffer */
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = & image_buffer[cinfo.next_scanline * row_stride];
+        (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+    free(image_buffer);
+    free(result);
+}
 
 int main(int argc, char **argv) {
     struct sockaddr_in tcp_host_address, udp_host_address, tcp_client_address;
@@ -153,15 +246,23 @@ void say_hello(struct sockaddr_in * tcp_host_address, struct sockaddr_in * udp_h
 }
 
 void handle_connection(int host_socket_tcp, int host_socket_udp, int client_socket_tcp, struct sockaddr_in * tcp_client_address, unsigned int access_code) {
+    struct sockaddr_in udp_client_address;
+    pid_t screen_broadcast_process;
+
     LOG("Client %s connected", inet_ntoa(tcp_client_address->sin_addr));
     if(authorize(client_socket_tcp, access_code) == 1) {
-        
+        set_udp_client_address(client_socket_tcp, tcp_client_address->sin_addr, &udp_client_address);
+        screen_broadcast_process = begin_screen_broadcast(host_socket_udp, &udp_client_address);
+        if(screen_broadcast_process == -1) {
+            LOG("An error occured while starting screen broadcast");
+            return;
+        }
     }
 }
 
 int authorize(int client_socket_tcp, unsigned int access_code) {
     LOG("Waiting for access code...");
-    unsigned int provided_access_code = read_access_code(client_socket_tcp);
+    unsigned int provided_access_code = read_integer(client_socket_tcp);
     LOG("Provided access code: %u", provided_access_code);
     if(provided_access_code == access_code) {
         LOG("Access granted");
@@ -171,17 +272,48 @@ int authorize(int client_socket_tcp, unsigned int access_code) {
     return 0;
 }
 
-unsigned int read_access_code(int client_socket_tcp) {
+void set_udp_client_address(int client_socket_tcp, struct in_addr client_addr, struct sockaddr_in * udp_client_address) {
+    LOG("Waiting for client udp port...");
+    unsigned int client_udp_port = read_integer(client_socket_tcp);
+    udp_client_address->sin_family = AF_INET;
+    udp_client_address->sin_addr = client_addr;
+    udp_client_address->sin_port = htons(client_udp_port);
+    LOG("Client udp address: %s:%d", inet_ntoa(udp_client_address->sin_addr), client_udp_port);
+}
+
+unsigned int read_integer(int client_socket_tcp) {
     char buffer[sizeof(unsigned int)];
     int total_readed_bytes = 0;
     int readed_bytes = 0;
     while(total_readed_bytes < sizeof(unsigned int)) {
         readed_bytes = recv(client_socket_tcp, buffer + total_readed_bytes, sizeof(unsigned int) - total_readed_bytes, 0);
         if(readed_bytes == -1) {
-            printf("Failure on read access code \n");
+            printf("Failure reading integer \n");
             exit(-1);
         }
         total_readed_bytes += readed_bytes;
     }
     return ntohl(*((unsigned int *) buffer));
+}
+
+int begin_screen_broadcast(int host_socket_udp, struct sockaddr_in * udp_client_address) {
+    LOG("Begin screen broadcast, creating child process...");
+    pid_t pid;
+    if((pid = fork()) == -1) {
+        LOG("fork() failed");
+        return -1;
+    }
+    if(pid == 0) {
+        while(1) {
+            send_frame(host_socket_udp, udp_client_address);
+            sleep(1);
+        }
+    }
+    return pid;
+}
+
+void send_frame(int host_socked_udp, struct sockaddr_in * udp_client_address) {
+    unsigned char * frame = "a";
+    unsigned long frame_size = 1;
+    sendto(host_socked_udp, frame, frame_size, 0, udp_client_address, sizeof(*udp_client_address));
 }
